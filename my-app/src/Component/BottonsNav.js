a8572cbdb133550021453094d5bddcb4839fb322
Johan794 <johan.ricardo1104@gmail.com>
2022-05-29 17:20:10 -0500

    tengo sueño

--- my-app/src/Component/BottonsNav.js
+++ my-app/src/Component/BottonsNav.js
@@ -9,56 +9,47 @@ import "./styles/NavBar.css";
 
 export function BottonsNav({ user, handleClick }) {
   const [value, setValue] = React.useState("recents");
-  const [clicked, setClicked] = React.useState(false);
+//  const [clicked, setClicked] = React.useState(false);
   const isbarber = user === "barber";
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <div>
-      {!clicked ? (
-        <BottomNavigation
-          sx={{ width: 500 }}
-          value={value}
-          onChange={handleChange}
-        >
-          {isbarber ? (
-            <BottomNavigationAction
-              label="Add a new client"
-              value="New client"
-              icon={<FavoriteIcon />}
-            />
-          ) : (
-            <BottomNavigationAction
-              label="New Appointment"
-              value="New Appointment"
-              icon={<FavoriteIcon />}
-            />
-          )}
-          <BottomNavigationAction
-            label="New barber info"
-            value="Add barber info"
-            icon={<LocationOnIcon />}
-          />
-          <BottomNavigationAction
-            label="Edit info"
-            value="Edit info"
-            icon={<BorderColorIcon />}
-          />
-          <BottomNavigationAction
-            label="Logout"
-            value="Logout"
-            icon={<LogoutIcon />}
-            onClick={() => {
-                console.log("clicked");
-                setClicked(true);
-            }}
-          />
-        </BottomNavigation>
+    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+      {isbarber ? (
+        <BottomNavigationAction
+          label="Add a new client"
+          value="New client"
+          icon={<FavoriteIcon />}
+        />
       ) : (
-        handleClick()
+        <BottomNavigationAction
+          label="New Appointment"
+          value="New Appointment"
+          icon={<FavoriteIcon />}
+        />
       )}
-    </div>
+      <BottomNavigationAction
+        label="New barber info"
+        value="Add barber info"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction
+        label="Edit info"
+        value="Edit info"
+        icon={<BorderColorIcon />}
+      />
+      <BottomNavigationAction
+        label="Logout"
+        value="Logout"
+        icon={<LogoutIcon />}
+        onClick={() => {
+          console.log("clicked");
+          localStorage.clear();
+          window.location.href = "/";
+        }}
+      />
+    </BottomNavigation>
   );
 }
--- my-app/src/Component/PageCards.js
+++ my-app/src/Component/PageCards.js
@@ -1,32 +1,59 @@
-import { Card } from '@mui/material';
-import * as React from 'react';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import "./styles/cards.css"
-
+import { Card } from "@mui/material";
+import * as React from "react";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import "./styles/cards.css";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 export const PageCards = () => {
-
   return (
-    <Card sx={{ maxWidth: 345 }} className="card">
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://th.bing.com/th/id/OIP.qc8aczpQp6Gzi9KD5aIrlQHaE8?pid=ImgDet&rs=1"
-          alt="barbero"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Barbero
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            barbers are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={6} md={8}>
+          <Card sx={{ maxWidth: 345 }} className="card">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://th.bing.com/th/id/OIP.qc8aczpQp6Gzi9KD5aIrlQHaE8?pid=ImgDet&rs=1"
+                alt="barbero"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Barbero
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  barbers are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={6} md={8}>
+          <Card sx={{ maxWidth: 345 }} className="card">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://source.unsplash.com/random/200x200?sig=1"
+                alt="barbero"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  otro Barbero
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  barbers are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
-}
+};
