a32579fb60c6afca72d9705071fc672ea1f24588
CamiloCJ09 <camilocampazj@gmail.com>
2022-05-31 19:37:14 -0500

    Working api

--- Api/userApi.py
+++ Api/userApi.py
@@ -1,6 +1,8 @@
 import uuid
 from flask import Blueprint, request, jsonify
 from firebase_admin import firestore
+import requests
+from requests.exceptions import HTTPError
 
 db = firestore.client()
 user_Ref = db.collection('user')
@@ -21,6 +23,7 @@ class Barber:
         return Barber()
     def addBarberAppointment(barber, appointment):
         barber.clients.append(appointment)
+    
 class Client:
     def __init__(self):
         self.appointments = []
@@ -29,16 +32,29 @@ class Client:
 def create():
     try:
         id = uuid.uuid4()    
-        user_Ref.document(id.hex).set(request.json)
+        user_Ref.document(request.json['id']).set(request.json)
         return jsonify({'status': True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
+@userApi.route('/test',methods=['GET','POST'])
+def test():
+
+    test = request.json
+    db.collection("user").document(test['id']).update({
+                "Name" : firestore.ArrayUnion(["Pedro"])
+            })
+    return jsonify({'status': test['Name']}), 200
 @userApi.route('/addAppointment', methods=['POST','GET'])
 def addAppo():
     try:
         if request.method == 'POST':
             id = uuid.uuid4()
+            db.collection("user").doccument(request.headers['name']).update({
+                "appointments" : firestore.ArrayUnion([request.headers])
+                
+            })
+            request.headers['appointments']
             user_Ref.document(id.hex).set(request.json)
             return jsonfy({'status': True}), 200
         elif request.method == 'GET':
